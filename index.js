262fdad61fa9f8fe98a3ddb66156193031f4c31d
Васлий Василик <vaslijvasilik@Vaslijs-MacBook-Pro.local>
2019-07-18 22:33:38 +0200

    refactor

--- index.js
+++ index.js
@@ -1,109 +1,102 @@
-const images = document.querySelectorAll('.js-image');
+const DEBOUNCE_TIME = 18;
+const MAX_ANGLE = 25;
 
-var defaultTiltOpts = {
-	debounceTime: 18,
-	maxAngle: 25,
-	ignoreAngle: 5
-};
+const image = document.querySelector('.js-image');
 
-images.forEach(item => {
-	var sprite = {width: parseInt(item.dataset.width), height: parseInt(item.dataset.height)};
-	var mediaAsset = $(item);
-	var imageHeight = Math.round(item.getClientRects()[0].height);
-	var imageWidth = mediaAsset.width();
-	var sizeKoef = imageWidth / sprite.width;
-	var ratioImage = imageWidth / imageHeight;
-	var adaptedSpriteHeight = sprite.height * sizeKoef;
-	var sensativeAnle = 2 * (defaultTiltOpts.maxAngle - defaultTiltOpts.ignoreAngle);
-	var frameHeight = imageHeight * sizeKoef;
-	var numFrames = Math.floor(sprite.height / frameHeight);
-	var framesInOneAngle = numFrames / sensativeAnle;
+var sprite = {width: parseInt(image.dataset.width), height: parseInt(image.dataset.height)};
+var mediaAsset = $(image);
+var imageHeight = Math.round(image.getClientRects()[0].height);
+var imageWidth = mediaAsset.width();
+var sizeKoef = imageWidth / sprite.width;
+var adaptedSpriteHeight = sprite.height * sizeKoef;
+var sensativeAngle = 2 * MAX_ANGLE;
+var frameHeight = imageHeight * sizeKoef;
+var numFrames = Math.floor(sprite.height / frameHeight);
+var framesInOneAngle = numFrames / sensativeAngle;
 
-	init();
+tilt();
 
-	$(window).on('deviceorientation', throttledTilt.bind(this));
+window.addEventListener('deviceorientation', throttledTilt.bind(this));
 
-	function init() {
-		tilt();
+function throttledTilt(e) {
+	return debounce(tilt.call(this, e), DEBOUNCE_TIME);
+}
+
+function setOffset(value) {
+	$(image).css({'background-position': '0 ' + (-1 * value)  + 'px'});
+}
+
+function getFloorOffset(opts) {
+	var offset = opts.offset - (opts.offset % imageHeight);
+
+	if (offset < 0) {
+		offset = 0;
+	} else if (offset > adaptedSpriteHeight- imageHeight) {
+		offset = adaptedSpriteHeight - imageHeight
 	}
+	return offset;
+}
 
-	function throttledTilt(e) {
-		return _.debounce(tilt.call(this, e), defaultTiltOpts.debounceTime);
-	}
-
-	function tilt(e) {
-		if (!e) {
-			setOffset(getFloorOffset({
-				offset: adaptedSpriteHeight / 2
-			}));
-
-			return;
-		}
-
-		var gamma = e.originalEvent.gamma;
-		var tilt = gamma - this.initialAngle;
-		var absoluteTilt = Math.abs(tilt);
-
-
-		if (_.isUndefined(this.initialAngle)) {
-			this.initialAngle = gamma;
-
-			setOffset(getFloorOffset({
-				offset: adaptedSpriteHeight / 2
-			}));
-
-			return;
-		}
-		
-		if (!isTiltInSensativeAre(absoluteTilt, this.initialAngle)) {
-			setOffset(getFloorOffset({
-				offset: adaptedSpriteHeight / 2
-			}));
-
-			return;
-		}
-
-		if (tilt < 0 && tilt < -defaultTiltOpts.maxAngle) {
-			setOffset(getFloorOffset({
-				offset: 0
-			}));
-
-			return;
-		}
-
-		if (tilt > 0 && tilt > defaultTiltOpts.maxAngle) {
-			setOffset(getFloorOffset({
-				offset: adaptedSpriteHeight
-			}));
-
-			return;
-		}
-
-		var offsetDirty = (Math.abs(tilt) - defaultTiltOpts.ignoreAngle) * framesInOneAngle * frameHeight;
-		var offset = tilt < 0
-			? adaptedSpriteHeight / 2 - offsetDirty
-			: adaptedSpriteHeight / 2 + offsetDirty
+function tilt(e) {
+	if (!e) {
 		setOffset(getFloorOffset({
-			offset: offset
+			offset: adaptedSpriteHeight / 2
 		}));
 
-		function setOffset(value) {
-			$(item).css({'background-position': '0 ' + (-1 * value)  + 'px'});
-		}
-
-		function getFloorOffset(opts) {
-			var offset = opts.offset - (opts.offset % imageHeight);
-
-			if (offset < 0) {
-				offset = 0;
-			} else if (offset > adaptedSpriteHeight- imageHeight) {
-				offset = adaptedSpriteHeight - imageHeight
-			}
-			return offset;
-		}
-
-		function isTiltInSensativeAre(absoluteTilt, initialAngle) {
-			return (-defaultTiltOpts.ignoreAngle > absoluteTilt) || (defaultTiltOpts.ignoreAngle < absoluteTilt)
-		}
+		return;
 	}
-})
+
+	var gamma = e.gamma;
+	var tilt = gamma - this.initialAngle;
+
+	if (!this.initialAngle) {
+		this.initialAngle = gamma;
+
+		setOffset(getFloorOffset({
+			offset: adaptedSpriteHeight / 2
+		}));
+
+		return;
+	}
+
+	if (tilt < 0 && tilt < -MAX_ANGLE) {
+		setOffset(getFloorOffset({
+			offset: 0
+		}));
+
+		return;
+	}
+
+	if (tilt > 0 && tilt > MAX_ANGLE) {
+		setOffset(getFloorOffset({
+			offset: adaptedSpriteHeight
+		}));
+
+		return;
+	}
+
+	var offsetDirty = Math.abs(tilt) * framesInOneAngle * frameHeight;
+	var offset = tilt < 0
+		? adaptedSpriteHeight / 2 - offsetDirty
+		: adaptedSpriteHeight / 2 + offsetDirty
+	setOffset(getFloorOffset({
+		offset: offset
+	}));
+}
+
+function debounce(func, wait, immediate) {
+	let timeout;
+
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
